6252e6fb7183d295f4ffff1bd9f02afff771be2d
Aadarsh <142106819+aadarshsingh27@users.noreply.github.com>
2026-01-12 23:18:05 +0530

    error fixed

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ErrorBoundary from './components/ErrorBoundary';
+
+console.log("Starting App...");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
